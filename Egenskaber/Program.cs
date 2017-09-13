9dcbe4ba4584cdfec1eb226047b4a02e0c3f254d
Asbjørn <admin@TA-8215-05>
2017-09-13 13:53:33 +0200

    .

---
+++ Egenskaber/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egenskaber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trekant tre = new Trekant(4, 3);
+            Console.WriteLine(tre.Areal);
+
+            Vare v1 = new Vare("Køler", 975);
+
+            Vare v2 = new Vare();
+            v2.Navn = "Køler";
+            v2.Pris = 975.55;
+            Console.WriteLine(v2.PrisMedMons());
+
+            Person p1 = new Person();
+            p1.Fornavn = "Asbjørn";
+            p1.Efternavn = "Jacobsen";
+            Console.WriteLine(p1.Fuldnavn());
+
+            Console.WriteLine(ArealBeregninger.BeregnArealFirkant(10, 10));
+
+            Console.WriteLine(ArealBeregninger.BeregnArealCirkel(10));
+        }
+        class Trekant
+        {
+            public int Grundlinje { get; private set; }
+            public int Højde { get; private set; }
+            public Trekant(int Grundlinje, int Højde)
+            {
+                this.Grundlinje = Grundlinje;
+                this.Højde = Højde;
+            }
+            public double Areal
+            {
+                get
+                {
+                   return this.Grundlinje * this.Højde * 0.5;
+                }
+            }
+            
+        }
+        class Vare
+        {
+            private string navn;
+            public string Navn
+            {
+                get
+                {
+                    Console.WriteLine("Nu aflæses navn");
+                    return this.navn;
+                }
+                set
+                {
+                    Console.WriteLine("Nu tildeles navn");
+                    this.navn = value;
+                }
+            }
+            private double pris;
+            public double Pris
+            {
+                get
+                {
+                    Console.WriteLine("Nu aflæses pris");
+                    return this.pris;
+                }
+                set
+                {
+                    Console.WriteLine("Nu tildeles pris");
+                    this.pris = value;
+                }
+            }
+            public Vare()
+            {
+
+            }
+            public Vare(string navn, double pris)
+            {
+                this.navn = navn;
+                this.pris = pris;
+            }
+            public double PrisMedMons()
+            {
+                return this.pris * 1.25;
+            }
+        }
+        class Person
+        {
+            public string Fornavn { get; set; }
+            private string efternavn;
+
+            public string Efternavn
+            {
+                get { return efternavn; }
+                set
+                {
+                    if (value.Length >= 3)
+                    {
+                        efternavn = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Efternavn for kort, minimum 3 karakterer");
+                        efternavn = "";
+                    }
+                }
+            }
+            public string Fuldnavn()
+            {
+                return this.Fornavn + " " + this.efternavn;
+            }
+            
+        }
+        static class ArealBeregninger
+        {
+            static public double BeregnArealFirkant(double højde, double brede)
+            {
+                return højde * brede;
+            }
+            static public double BeregnArealCirkel(double radius)
+            {
+                return Math.PI * radius * radius;
+            }
+        }
+    }
+}
---
+++ Nedarvning/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nedarvning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Terning t1 = new Terning(5);
+            //Console.WriteLine(t1.Værdi);
+            //t1.Ryst();
+            //Console.WriteLine(t1.Værdi);
+
+            //LudoTerning t2 = new LudoTerning();
+            ////Console.WriteLine(t2.Værdi + " " + t2.ErGlobus() + " " + t2.ErStjerne());
+            ////t2.Ryst();
+            ////Console.WriteLine(t2.Værdi + " " + t2.ErGlobus() + " " + t2.ErStjerne());
+            //t2.Ryst();
+            //Console.WriteLine(t2.Værdi + " " + t2.ErGlobus() + " " + t2.ErStjerne());
+            //t2.Ryst();
+            //Console.WriteLine(t2.Værdi + " " + t2.ErGlobus() + " " + t2.ErStjerne());
+            //t2.Ryst();
+            //Console.WriteLine(t2.Værdi + " " + t2.ErGlobus() + " " + t2.ErStjerne());
+
+            LudoTerning l = new LudoTerning();
+            l.Skriv();
+
+            Terning[] terninger = new Terning[4];
+            terninger[0] = new Terning();
+            terninger[1] = new LudoTerning();
+            terninger[2] = new LudoTerning();
+            terninger[3] = new Terning();
+            foreach (var item in terninger)
+            {
+                item.Skriv();
+            }
+        }
+    }
+    class Person
+    {
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+        public string FuldtNavn()
+        {
+            return Fornavn + " " + Efternavn;
+        }
+    }
+    class Elev : Person
+    {
+        public string KlasseLokale { get; set; }
+    }
+    class Instruktør : Person
+    {
+        public int NøgleId { get; set; }
+    }
+    class Udvidetrandom : Random
+    {
+        public bool NextBool()
+        {
+            return this.Next(1, 1001) <= 500;
+        }
+    }
+    class Terning
+    {
+        private int værdi;
+        public int Værdi
+        {
+            get
+            {
+                //Console.WriteLine("Terning aflæses som " + this.værdi);
+                return this.værdi;
+            }
+            set
+            {
+                if (1<= value && 6>= value)
+                {
+                    //Console.WriteLine("Terning tildeles værdien "+ value);
+                    this.værdi = value;
+                }
+                else
+                {
+                    //Console.WriteLine("Terning tildeles værdien " + 1);
+                    this.værdi = 1;
+                }
+                
+            }
+        }
+        private static Random rnd = new Random();
+        public void Ryst()
+        {
+            this.Værdi = rnd.Next(1, 7);
+        }
+        public virtual void Skriv()
+        {
+            Console.Write("[" + this.værdi + "]");
+        }
+        public Terning()
+        {
+            this.Ryst();
+        }
+        public Terning(int værdi)
+        {
+            this.Værdi = værdi;
+        }
+    }
+    class LudoTerning : Terning
+    {
+        public bool ErGlobus()
+        {
+            return this.Værdi == 3;
+        }
+        public bool ErStjerne()
+        {
+            return this.Værdi == 5;
+        }
+        public LudoTerning()
+        {
+
+        }
+        public LudoTerning(int værdi) :base(værdi)
+        {
+            this.Værdi = værdi;
+        }
+        public override void Skriv()
+        {
+            if (ErStjerne())
+            {
+                Console.Write("[S]");
+            }
+            else if (ErGlobus())
+            {
+                Console.Write("[G]");
+            }
+            else
+            {
+                base.Skriv();
+            }
+            
+        }
+    }
+}
